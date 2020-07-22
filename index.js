08ba931068309cca0ce9d37be329f1cdd3aadcec
Jacob McCracken <mccrackenjacob@gmail.com>
2020-07-22 04:11:06 +0000

    added Andrei's solution and attempted to utilize encapsulation. I'm not there yet apparently.

--- index.js
+++ index.js
@@ -1,53 +1,94 @@
 // goal is to create a function that can only run once!
 
-starter function:
-let view;
-function initialize() {
-  view = 'hello';
-  console.log('view has been set!')
-}
+// starter function:
+// let view;
+// function initialize() {
+//   view = 'hello';
+//   console.log('view has been set!')
+// }
 
-initialize();
-initialize();
-initialize();
-console.log(view)
+// initialize();
+// initialize();
+// initialize();
+// console.log(view)
 
 // Attempted solution:
 //  --> remove view from global scope 
-function initialize2() {
-  let view;
-  return function setView() {
-    view = 'hello';
-    console.log('view has been set w/ initialize2')
-  }
-}
+// function initialize2() {
+//   let view;
+//   return function setView() {
+//     view = 'hello';
+//     console.log('view has been set w/ initialize2')
+//   }
+// }
 
-initialize2()()
-initialize2()()
-initialize2()()
-console.log(view)
+// initialize2()()
+// initialize2()()
+// initialize2()()
+// console.log(view)
 
 // function can still run multiple times.... Why? View is declared in the parent scope of setView.... would altering the scoping solve this?
 
 // declare view after we update it's value? (also, switching to arrow fn's because I love them)
 
-const initialize3 = () => {
-  view = 'maybe?'
-  return () => {
-    let view;
-    console.log(`${view}`)
-  }
-}
+// const initialize3 = () => {
+//   view = 'maybe?'
+//   return () => {
+//     let view;
+//     console.log(`${view}`)
+//   }
+// }
 
-initialize3()();
-console.log(view);
+// initialize3()();
+// console.log(view);
 
 // this also doesn't solve the problem..... Is scope the wrong angle to approach from?
 
 // Andrei's solution:
 
-let view;
-function initializeSolved() {
-  
+// let view;
+// function initializeSolved() {
+//   let called = 0;
+//   return function() {
+//     if (called > 0) {
+//       return;
+//     } else {
+//       view = 'solved';
+//       called++;
+//       console.log('solved the problem!')
+//     }
+//   }
+// }
+
+// scoping was not entirely the wrong idea. Andrei solved this problem by utilizing a counter to trigger a conditional inside a nested function.
+
+// const startOnce = initializeSolved();
+// startOnce();
+// startOnce();
+// startOnce();
+// console.log(view)
+
+// one problem still remains though. initializeSolved can still be called. How can we solve this with encapsulation?
+
+// if we place it inside an object?
+
+const startOnce2 = {
+  view: '',
+  initialize: () => {
+    let called = 0;
+  return function() {
+    if (called > 0) {
+      return;
+    } else {
+      startOnce2.view = 'solved';
+      called++;
+      console.log('solved the problem!')
+    }
+  }
+  },
 }
 
+startOnce2.initialize()();
+startOnce2.initialize()();
+startOnce2.initialize()();
+// doesn't solve the problem.....
